9f78da5232be310fa1ac5569c4c6d86af95c00a5
Roman Rosluk <roma@redbox.by>
2017-09-07 11:23:43 +0200

    no file data fix

--- src/ios/ImageResizer.swift
+++ src/ios/ImageResizer.swift
@@ -17,15 +17,14 @@ import Foundation
             let dstWidth = command.arguments[8] as? Double ?? 0.0;
             let dstHeight = command.arguments[9] as? Double ?? 0.0;
             
-            
             var pluginResult: CDVPluginResult
-            
+
             pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_OK,
                 messageAs: true
             );
 
-            
+
             //print("Crearing url");
             let url = URL(string: src);
             let data: Data;
@@ -43,32 +42,41 @@ import Foundation
                 );
                 return;
             }
-            
+
             let srcImage = UIImage(data: data as Data);
-            
-            let srcWidth: Double = Double((srcImage?.size.width)!);
-            let srcHeight: Double = Double((srcImage?.size.height)!);
-            
-            let ratio: Double = min(dstWidth / srcWidth, dstHeight / srcHeight);
-            
-            let newWidth = srcWidth * ratio;
-            let newHeight = srcHeight * ratio;
-            
-            let newRect: CGRect = CGRect(x: 0.0, y: 0.0, width: CGFloat(newWidth), height: CGFloat(newHeight));
-            UIGraphicsBeginImageContext(newRect.size);
-            srcImage?.draw(in: newRect);
-            
-            let dstImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
-            UIGraphicsEndImageContext();
-            do {
-                try UIImagePNGRepresentation(dstImage)?.write(to: URL(string: dst)!);
-            } catch {
+
+            if (srcImage != nil) {
+                let srcWidth: Double = Double((srcImage?.size.width)!);
+                let srcHeight: Double = Double((srcImage?.size.height)!);
+
+                let ratio: Double = min(dstWidth / srcWidth, dstHeight / srcHeight);
+
+                let newWidth = srcWidth * ratio;
+                let newHeight = srcHeight * ratio;
+
+                let newRect: CGRect = CGRect(x: 0.0, y: 0.0, width: CGFloat(newWidth), height: CGFloat(newHeight));
+                UIGraphicsBeginImageContext(newRect.size);
+                srcImage?.draw(in: newRect);
+
+                let dstImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
+                UIGraphicsEndImageContext();
+                do {
+                    try UIImagePNGRepresentation(dstImage)?.write(to: URL(string: dst)!);
+                } catch {
+                    pluginResult = CDVPluginResult(
+                        status: CDVCommandStatus_ERROR,
+                        messageAs: "File was not saved"
+                    );
+                }
+
+            } else {
                 pluginResult = CDVPluginResult(
                     status: CDVCommandStatus_ERROR,
-                    messageAs: "File was not saved"
+                    messageAs: "File is not an image"
                 );
             }
-            //let imageDate:Data = UIImageJPEGRepresentation(dstImage, 1.0)!;
+
+                        //let imageDate:Data = UIImageJPEGRepresentation(dstImage, 1.0)!;
             
             
             //print(srcWidth, srcHeight, dstWidth, dstHeight, ratio, newWidth, newHeight, dstImage.size);
